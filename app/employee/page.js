03d0f70abea4c6f6e8ce978b57d75f91d6823395
Maitheryan-Sparkout <maithareyan@sparkouttech.com>
2026-06-22 13:20:03 +0530

    updated login

--- app/employee/page.js
+++ app/employee/page.js
@@ -1,7 +1,3 @@
-import { getServerSession } from "next-auth";
-import { redirect } from "next/navigation";
-import { authOptions } from "../api/auth/[...nextauth]/route";
-
 import DepartementFilter from "../components/DepartmentFilter";
 import EmployeeCard from "../components/EmployeeCard";
 import Navbar from "../components/Navbar";
@@ -9,12 +5,7 @@ import SearchBar from "../components/SearchBar";
 
 
 export default async function Employee({searchParams}) {
-      const session = await getServerSession(authOptions);
 
-    if (!session) {
-        redirect("/");
-    }
-    
     const{department}=await searchParams;
 
     const res =await fetch("https://dummyjson.com/users");
