04909ac0322d9ebc4a32532a543bab8104553d4f
Maitheryan-Sparkout <maithareyan@sparkouttech.com>
2026-06-23 17:47:17 +0530

    update pagination

--- app/employee/page.js
+++ app/employee/page.js
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 import DepartementFilter from "../components/DepartmentFilter";
 import EmployeeCard from "../components/EmployeeCard";
 import Navbar from "../components/Navbar";
@@ -5,11 +7,18 @@ import SearchBar from "../components/SearchBar";
 
 
 export default async function Employee({searchParams}) {
+    const params=await searchParams;
 
-    const{department}=await searchParams;
+    const page=Number(params.page) || 1;
+    const department=params.department;
 
-    const res =await fetch("https://dummyjson.com/users");
+    const limit =10;
+    const skip=(page-1) * limit;
+
+    const res =await fetch(`https://dummyjson.com/users?limit=${limit}&skip=${skip}`,{cache: "no-store"});
+
     const data = await res.json();
+const totalPages=Math.ceil(data.total/limit);
 
     let employee=data.users;
 
@@ -36,6 +45,24 @@ export default async function Employee({searchParams}) {
                     />
                 )
                 )}
+            </div>
+            <div className="page" style={{marginTop:"20px"}}>
+                {page >1 && (
+                <Link href={`/employee?page=${page -1}`}>
+                Previous
+                </Link>
+                )}
+
+                <span style={{margin: "0 20px"}}>
+                    Page {page} of {totalPages}
+                </span>
+
+                {page < totalPages && (
+                    <Link href={`/employee?page=${page +1}`}>
+                Next
+                </Link>
+                )}
+
             </div>
         </div>
         
